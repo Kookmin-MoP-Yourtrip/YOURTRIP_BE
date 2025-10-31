1e80698b34fa09ac07583a3550fff012016198ef
zie-ning <jeeun03@gmail.com>
2025-10-31 16:21:46 +0900

    Feat: BaseEntity 작성 및 jpa auditing 활성화

--- yourtrip/src/main/java/backend/yourtrip/YourtripApplication.java
+++ yourtrip/src/main/java/backend/yourtrip/YourtripApplication.java
@@ -2,8 +2,10 @@ package backend.yourtrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class YourtripApplication {
 
     public static void main(String[] args) {
