8d164be7735c5aca31d0941cbb973c9b90adb746
taehwan <th2194@naver.com>
2025-11-18 02:15:05 +0900

    feature/mypage 수정

--- yourtrip/src/main/java/backend/yourtrip/domain/mypage/dto/response/MyFeedVisibilityResponse.java
+++
@@ -1,7 +0,0 @@
-package backend.yourtrip.domain.mypage.dto.response;
-
-public record MyFeedVisibilityResponse(
-    Long feedId,
-    boolean isPublic,
-    String message
-) {}
--- yourtrip/src/main/java/backend/yourtrip/domain/notification/entity/enums/NotificationType.java
+++
@@ -1,7 +0,0 @@
-package backend.yourtrip.domain.notification.entity.enums;
-
-public enum NotificationType {
-    COMMENT,        // 내 피드에 댓글 달림
-    COURSE_REMIND,  // 코스 일정 리마인드
-    COURSE_INVITE   // 코스 초대 알림
-}
